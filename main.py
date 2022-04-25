a70a6e5d849b538f97ea53223b184afd27a877e0
Simon Rey <reysimon@orange.fr>
2022-04-25 14:24:49 +0200

    Reorganising

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from data_generation import *
 from maj_dynamics import *
-from experiment import *
+from experiments import *
 from plot import *
 
 # Returns a generator for random profiles
@@ -12,17 +12,23 @@ def randomProfiles(numAgents, numAlts, numProfiles):
 def allProfiles(numAgents, numAlts, numProfiles):
 	return getAllProfiles(numAgents, numAlts)
 
+##### TO CREATE EXPERIMENTS DATA
+
 # dataManipulation = runManipulationExperiment(11, 20000)
 # (dataNumAgents, dataCompleteness) = runFrequencyExperiment(25, 5000000, numAgentsMin = 17)
 
-dataNumAgents = pd.read_pickle("frequencyNumAgentsData_5000000_25.pkl")
-dataCompleteness = pd.read_pickle("frequencyCompletenessData_5000000_25.pkl")
-dataManipulation = pd.read_pickle("manipulationData_allOrders_20000.pkl")
+##### READ EXPERIMENT DATA AND GENERATE THE FINAL PLOTS
+
+dataNumAgents = pd.read_pickle("expeData/frequencyNumAgentsData_5000000_25.pkl")
+dataCompleteness = pd.read_pickle("expeData/frequencyCompletenessData_5000000_25.pkl")
+dataManipulation = pd.read_pickle("expeData/manipulationData_allOrders_20000.pkl")
 
 frequencyNumAgentsPlot(dataNumAgents, 5, criterias = [majUndom, plurUndom, condorcet], fileName = "Plots/finalFreqNumAgents.pdf")
 frequencyCompletenessPlot(dataCompleteness, 15, 5, criterias = [majUndom, plurUndom, condorcet], fileName = "Plots/finalFreqCompleteness.pdf")
 manipulationPlot(dataManipulation, criterias = [majUndom, plurUndom, condorcet], fileName = "Plots/finalManipulation.pdf")
 
-generateFrequencyNumAgentsPlots(dataNumAgents, 5)
-generateFrequencyCompletenessPlots(dataCompleteness, 15, 5)
-generateManipulationPlots(dataManipulation)
+##### TO GENERATE ALL THE PLOTS
+
+# generateFrequencyNumAgentsPlots(dataNumAgents, 5)
+# generateFrequencyCompletenessPlots(dataCompleteness, 15, 5)
+# generateManipulationPlots(dataManipulation)
